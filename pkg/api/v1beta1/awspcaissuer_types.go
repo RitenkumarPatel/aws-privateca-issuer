c14cc2e0f313ab61d89ea2c0b82537bd9f05ebf8
Riten <ec2-user@ip-172-31-30-172.us-east-2.compute.internal>
2025-07-03 18:21:21 +0000

    Attach template names to issuers
    The charts were updated to accept a pcaTemplateName that maps to a
    template ARN. There is a new example showing this. The template mapping
    was updated to reflect this. The controllers were adjusted to call the
    updated function. The unit tests were updated to validate the
    BuildTemplateArn function,
    
    Change templateArn to pcaTemplateName in crds

--- pkg/api/v1beta1/awspcaissuer_types.go
+++ pkg/api/v1beta1/awspcaissuer_types.go
@@ -37,6 +37,9 @@ type AWSPCAIssuerSpec struct {
 	// Needs to be specified if you want to authorize with AWS using an access and secret key
 	// +optional
 	SecretRef AWSCredentialsSecretReference `json:"secretRef,omitempty"`
+	// Specifies the template name of the issuer
+	// +optional
+	TemplateArn string `json:"templateArn,omitempty"`
 }
 
 // AWSCredentialsSecretReference defines the secret used by the issuer
